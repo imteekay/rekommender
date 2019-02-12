8d8387a56fd61d2a798db45b9d4ead3c8a66ffed
TK <leandrotk100@gmail.com>
2019-02-12 06:40:10 -0200

    Add cheshire

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.25"]
                  [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.25"]
+                 [cheshire "5.8.1"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "nukr.server/run-dev"]}
