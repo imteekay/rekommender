e0039709dfd6f96eda5c2256671ba9f685386cdd
TK <leandrotk100@gmail.com>
2019-02-12 06:40:41 -0200

    Implement profile create and list API endpoint

--- src/nukr/service.clj
+++ src/nukr/service.clj
@@ -4,15 +4,62 @@
             [io.pedestal.http.body-params :as body-params]
             [ring.util.response :as ring-resp]))
 
-(defn get-profiles
-  [request]
-  (ring-resp/response {:profiles [{:id 1 :username "TK" :opt-in true}
-                                  {:id 2 :username "Kazu" :opt-in true}
-                                  {:id 3 :username "Kaio" :opt-in true}]}))
+(defonce database (atom {}))
 
-(def common-interceptors [(body-params/body-params) http/html-body])
+(defn response [status body & {:as headers}]
+  {:status status :body body :headers headers})
 
-(def routes #{["/api/profiles" :get (conj common-interceptors `get-profiles)]})
+(def ok       (partial response 200))
+(def created  (partial response 201))
+(def accepted (partial response 202))
+
+(defn add-profile
+  [new-profile]
+  (if-let [profiles (get @database :profiles)]
+    (conj profiles new-profile)
+    [new-profile]))
+
+(def db-interceptor
+  {:name :database-interceptor
+   :enter (fn [context]
+            (update context :request assoc :database @database))
+   :leave (fn [context]
+            (let [args   (:tx-data context)
+                  table  (first (map key args))
+                  params (get args table)]
+              (if (and args table params)
+                (do
+                  (swap! database assoc table (add-profile params))
+                  (assoc-in context [:request :database] @database))
+                context)))})
+
+(defn make-profile
+  [id username]
+  {:id       id
+   :username username
+   :opt-in   true})
+
+(def profile-create
+  {:name :profile-create
+   :enter (fn [context]
+            (let [username    (get-in context [:request :json-params :username] "Unnamed Profile")
+                  db-id       (str (gensym "l"))
+                  new-profile (make-profile db-id username)]
+              (assoc context
+                     :response (created new-profile)
+                     :tx-data {:profiles new-profile})))})
+
+(def profiles-list
+  {:name :profiles-list
+   :enter (fn [context]
+            (if-let [profiles (get-in context [:request :database :profiles])]
+              (assoc context :response (ok profiles))
+              (assoc context :response (ok []))))})
+
+(def routes
+  (route/expand-routes
+   #{["/api/profiles" :post [db-interceptor (body-params/body-params) http/json-body profile-create]]
+     ["/api/profiles" :get [db-interceptor http/json-body profiles-list]]}))
 
 (def service {:env :prod
               ::http/routes routes
@@ -22,4 +69,3 @@
               ::http/container-options {:h2c? true
                                         :h2? false
                                         :ssl? false}})
-
--- test/nukr/service_test.clj
+++ test/nukr/service_test.clj
@@ -2,38 +2,43 @@
   (:require [clojure.test :refer :all]
             [io.pedestal.test :refer :all]
             [io.pedestal.http :as bootstrap]
+            [cheshire.core :as json]
             [nukr.service :as service]))
 
 (def service
   (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
 
-(deftest home-page-test
-  (is (=
-       (:body (response-for service :get "/"))
-       "Hello World!"))
-  (is (=
-       (:headers (response-for service :get "/"))
-       {"Content-Type" "text/html;charset=UTF-8"
-        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-        "X-Frame-Options" "DENY"
-        "X-Content-Type-Options" "nosniff"
-        "X-XSS-Protection" "1; mode=block"
-        "X-Download-Options" "noopen"
-        "X-Permitted-Cross-Domain-Policies" "none"
-        "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
+(defn parse-response-body
+  [response]
+  (json/parse-string (:body response) true))
 
-(deftest about-page-test
-  (is (.contains
-       (:body (response-for service :get "/about"))
-       "Clojure 1.9"))
-  (is (=
-       (:headers (response-for service :get "/about"))
-       {"Content-Type" "text/html;charset=UTF-8"
-        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-        "X-Frame-Options" "DENY"
-        "X-Content-Type-Options" "nosniff"
-        "X-XSS-Protection" "1; mode=block"
-        "X-Download-Options" "noopen"
-        "X-Permitted-Cross-Domain-Policies" "none"
-        "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
+(deftest api-integration-test
+  (testing "API Integration Test"
+    (let [response (response-for service
+                                 :get
+                                 "/api/profiles"
+                                 :headers {"Content-Type" "application/json"
+                                           "Accept" "application/json"})]
+      (is (= 200 (:status response)))
+      (is (= [] (parse-response-body response))))
 
+    (let [username "TK"
+          profile {:username username}
+          response (response-for service
+                                 :post
+                                 "/api/profiles"
+                                 :body (json/encode profile)
+                                 :headers {"Content-Type" "application/json"
+                                           "Accept" "application/json"})]
+      (is (= username (:username (parse-response-body response))))
+      (is (= 201 (:status response))))
+
+    (let [response (response-for service
+                                 :get
+                                 "/api/profiles"
+                                 :headers {"Content-Type" "application/json"
+                                           "Accept" "application/json"})
+          parsed-response-body (parse-response-body response)]
+      (is (= 200 (:status response)))
+      (is (not-empty parsed-response-body))
+      (is (= "TK" (-> parsed-response-body first :username))))))
